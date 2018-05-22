36fc1973c37baf27dbd3390e8a03f46f52a80bd9
Kyle Burrows <kylbrrws@gmail.com>
2018-05-22 17:28:08 -0400

    Extracted LDAPParameter to a new file

---
+++ LDAPClient/LDAPParameter.cs
@@ -0,0 +1,65 @@
+﻿namespace System.DirectoryServices
+{
+    public class LDAPParameter
+	{
+		public LDAPParameter() { }
+		public LDAPParameter(string name, string value)
+		{
+			Name = name;
+			Value = value;
+		}
+
+		private string m_Name { get; set; }
+		public string Name
+		{ 
+			get
+			{
+				return m_Name;
+			}
+			set
+			{
+				// replace these values for security reasons
+				m_Name = value
+					.Replace(@",", @"\,")
+					.Replace(@"\", @"\\")
+					.Replace(@"#", @"\#")
+					.Replace(@"+", @"\+")
+					.Replace(@"<", @"\<")
+					.Replace(@">", @"\>")
+					.Replace(@";", @"\;")
+					.Replace("\"", "\\\"")
+					.Replace(@"=", @"\=")
+					.Trim();
+			}
+		}
+
+		private string m_Value;
+		public string Value 
+		{ 
+			get
+			{
+				return m_Value;
+			}
+			set
+			{
+				// replace these values for security reasons
+				m_Value = value
+					.Replace(@",", @"\,")
+					.Replace(@"\", @"\\")
+					.Replace(@"#", @"\#")
+					.Replace(@"+", @"\+")
+					.Replace(@"<", @"\<")
+					.Replace(@">", @"\>")
+					.Replace(@";", @"\;")
+					.Replace("\"", "\\\"")
+					.Replace(@"=", @"\=")
+					.Trim();
+			}
+		}
+
+		public override string ToString()
+		{
+			return Name + "=" + Value;
+		}
+	}
+}
