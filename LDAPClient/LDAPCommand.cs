88252190b9db07a5f9a56f9939b04f19c24adf48
Kyle Burrows <kylbrrws@gmail.com>
2018-07-01 22:04:38 -0400

    Shelving

--- LDAPClient/LDAPCommand.cs
+++ LDAPClient/LDAPCommand.cs
@@ -2,11 +2,22 @@ using System.Collections.Generic;
 
 namespace System.DirectoryServices.LDAPClient
 {
-    public class LDAPCommand : ILDAPCommand
+    public class LDAPCommand : ILDAPCommand, IDisposable
     {
-		public List<LDAPParameter> Parameters { get; set; }
-		
-		public override string ToString()
+        DirectorySearcher ds;
+
+        public LDAPCommand(LDAPConnection connection)
+        {
+            Connection = connection;
+        }
+        public LDAPCommand()
+        {
+        }
+
+        LDAPConnection Connection { get; set; }
+        public List<LDAPParameter> Parameters { get; set; }
+
+        public override string ToString()
 		{
 			List<string> strParameters = new List<string>();
 
@@ -15,5 +26,27 @@ namespace System.DirectoryServices.LDAPClient
 
 			return string.Join(",", strParameters);
 		}
+
+        public SearchResultCollection FindAll()
+        {
+            return ds.FindAll();
+        }
+        public SearchResult FindOne()
+        {
+            return ds.FindOne();
+        }
+
+        /// <summary>
+        /// Get the underlying DirectorySearcher object
+        /// </summary>
+        /// <returns></returns>
+        public DirectorySearcher GetDirectorySearcher()
+        {
+            return ds;
+        }
+        public void Dispose()
+        {
+             ds.Dispose();
+        }
 	}
 }
---
+++ LDAPClient/LDAPConnection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.DirectoryServices.LDAPClient
+{
+    public class LDAPConnection : IDisposable
+    {
+        private void _LDAPConnection(Uri uri, string username = null, string password = null, AuthenticationTypes? authenticationType = null)
+        {
+            Username = username;
+            Password = password;
+            if(authenticationType != null)
+                AuthenticationType = (AuthenticationTypes)authenticationType;
+        }
+        public LDAPConnection(Uri uri, string username = null, string password = null, AuthenticationTypes? authenticationType = null)
+        {
+            _LDAPConnection(uri, username, password, authenticationType);
+        }
+        public LDAPConnection(string url, string username = null, string password = null, AuthenticationTypes? authenticationType = null)
+        {
+            var uri = new Uri(url);
+            _LDAPConnection(uri, username, password, authenticationType);
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public Uri Uri { get; set; }
+
+        public AuthenticationTypes AuthenticationType { get; set; }
+
+        DirectoryEntry de;
+
+        private void Init()
+        {
+            de = new DirectoryEntry();
+            
+            de.Username = Username;
+            de.Password = Password;
+            de.Path = Uri.AbsoluteUri.Replace("ldap://", "LDAP://");
+            de.AuthenticationType = AuthenticationType;
+        }
+        public void Dispose()
+        {
+            de.Dispose();
+        }
+
+        /// <summary>
+        /// Get the underlying DirectoryEntry object
+        /// </summary>
+        /// <returns></returns>
+        public DirectoryEntry GetDirectoryEntry()
+        {
+            return de;
+        }
+    }
+}
